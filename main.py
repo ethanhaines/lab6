37119c38d5622a8bba014816d9d1df72052b860a
ethanhaines <eboyhaines@gmail.com>
2023-10-25 13:59:00 -0400

    created

---
+++ main.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    encoded = None
+    while True:
+        password = input('Please enter the password you would like to encode/decode: ')
+        print("Menu")
+        print("------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        response = int(input('Which option would you like to choose? '))
+        if response == 1:
+            print(encode(password))
+        if response == 2:
+            print(decode(password))
+        if response == 3:
+            exit()
+
+
+if __name__ == '__main__':
+    main()
